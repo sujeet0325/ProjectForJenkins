abd8036e107e50a53b740d388fa5a9e08ee57631
sujeet0325 <142496120+sujeet0325@users.noreply.github.com>
2023-11-24 13:20:55 +0530

    Initial commit

---
+++ ProjectForJenkins/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForJenkins
+{
+    internal class Program
+    {
+        public void Display()
+        {
+            Console.WriteLine( "Welcome to Jenkins");
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.Display();
+            Console.ReadLine();
+        }
+    }
+}
